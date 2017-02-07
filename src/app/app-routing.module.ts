af4a692b9a3fb0f5567f9c901581dc96c60eb732
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-07 12:16:30 +0100

    edit profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import {NgModule} from "@angular/core";
 import {SuccesLoginComponent} from "./succesLogin.component";
 import {HomePageComponent} from "./homepage/homepage.component";
 import {PageNotFoundComponent} from "./PageNotFound.component";
+import {ProfileEditComponent} from "./profile/profileEdit.component.ts";
 
 export const routes: Routes = [
   {path: 'succesLogin', component: SuccesLoginComponent},
+  {path: 'profileEdit', component: ProfileEditComponent},
   {path: '', component: HomePageComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
@@ -16,4 +18,5 @@ export const routes: Routes = [
 })
 
 export class AppRoutingModule {
+
 }
---
+++ src/app/profile/profileEdit.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'profileEdit',
+  templateUrl: 'profileEdit.component.html',
+  styleUrls: ['profileEdit.component.css']
+})
+
+export class ProfileEditComponent {
+
+}
