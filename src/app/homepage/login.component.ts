66fb5813aed5cc851a14872cc965adf262943cab
jellemannaerts <jelle.mannaerts@gmail.com>
2017-02-09 15:19:18 +0100

    checking empty username, password

--- src/app/homepage/login.component.ts
+++ src/app/homepage/login.component.ts
@@ -13,29 +13,51 @@ export class LoginComponent {
   constructor(private auth: AuthService) {
   }
 
+  allFieldsFilledIn(username, password): boolean {
+    if (username == '' && password == '') {
+      this.messages.errorMessage = 'Username and password has to be filled in.';
+      this.messages.succesMessage = '';
+      return false;
+    } else if (username == '') {
+      this.messages.errorMessage = 'Username has to be filled in.';
+      this.messages.succesMessage = '';
+      return false;
+    } else if (password == '') {
+      this.messages.errorMessage = 'Password has to be filled in.';
+      this.messages.succesMessage = '';
+      return false;
+    }
+    return true;
+  }
+
   loginWithErrorHandling(username, password) {
-    this.auth.login(username, password).subscribe(data => {
-      console.log('login succes');
-      this.messages.errorMessage = '';
-      this.messages.succesMessage = '';
-    }, err => {
-      console.log('login error');
-      this.messages.errorMessage = 'Username and password need to be filled in.';
-      this.messages.succesMessage = '';
-    });
+    if (this.allFieldsFilledIn(username, password)) {
+      this.auth.login(username, password).subscribe(data => {
+        console.log('login succes');
+        this.messages.errorMessage = '';
+        this.messages.succesMessage = '';
+      }, err => {
+        console.log('login error');
+        this.messages.errorMessage = 'An error has occurred while logging in.';
+        this.messages.succesMessage = '';
+      });
+    }
   }
 
   signUpWithErrorHandling(username, password) {
-    this.auth.signup(username, password).subscribe(data => {
-      console.log('signup succes');
-      this.messages.errorMessage = '';
-      this.messages.succesMessage = 'A verification email will be sent.';
-      }, err => {
-      console.log('signup error');
-      this.messages.errorMessage = 'An error has occurred whilst signing up.';
-      this.messages.succesMessage = '';
-      }
-    );
+    if (this.allFieldsFilledIn(username, password)) {
+
+      this.auth.signup(username, password).subscribe(data => {
+          console.log('signup succes');
+          this.messages.errorMessage = '';
+          this.messages.succesMessage = 'A verification email will be sent.';
+        }, err => {
+          console.log('signup error');
+          this.messages.errorMessage = 'An error has occurred while signing up.';
+          this.messages.succesMessage = '';
+        }
+      );
+    }
   }
 }
 
