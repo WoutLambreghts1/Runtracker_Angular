9d490e1a85bcc9337a00dcd7147f43eede52fe42
jellemannaerts <jelle.mannaerts@gmail.com>
2017-02-12 13:09:16 +0100

    create new user if it doesnt exist

--- src/app/user/home/userHomepage.service.ts
+++ src/app/user/home/userHomepage.service.ts
@@ -1,28 +1,56 @@
 import {Injectable} from '@angular/core';
-import {Http,Headers,Response} from '@angular/http';
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import {User} from "./../../model/user";
 import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class UserHomepageService {
+  private BASEURL = 'http://localhost:8080';
 
-  constructor(private http:Http){}
+  constructor(private http: Http) {
+  }
 
-  getUser(): Observable<User>{
+  storeUserTokens(url: string) {
+    localStorage.setItem('access_token', url.split('=')[1].split('&')[0]);
+    localStorage.setItem('id_token', url.split('=')[5]);
+  }
+
+  getUser(): Observable<User> {
     var jwt = localStorage.getItem('id_token');
     var authHeader = new Headers();
-    if(jwt) {
+    if (jwt) {
       authHeader.append('token', jwt);
     }
-    return this.http.get('http://localhost:8080/api/users/getUser', {
-          headers: authHeader
-        })
-        .map((res: Response) => res.json())
-        .catch(this.handleErrorObservable)
+    return this.http.get(this.BASEURL + '/api/users/getUser', {
+      headers: authHeader
+    })
+      .map((res: Response) => res.json())
+      .catch(this.handleUserError)
   }
-  
 
-  private handleErrorObservable(error: Response | any): Observable<any> {
+  private handleUserError(error: Response | any): Observable<any> {
+    if (error.status == 404) {
+      return this.createUser();
+    } else {
+      return this.handleError(error);
+    }
+  }
+
+  createUser(): Observable<User> {
+    var jwt = localStorage.getItem('id_token');
+    let headers = new Headers({'Content-Type': 'application/json'});
+    if (jwt) {
+      headers.append('token', jwt);
+    }
+    let options = new RequestOptions({headers: headers});
+
+    let newUser = new User();
+
+    return this.http.post(this.BASEURL + '/api/users/createUser', newUser, options)
+      .map((res: Response) => res.json()).catch(err => this.handleError(err));
+  }
+
+  private handleError(error: Response | any): Observable<any> {
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
@@ -31,8 +59,6 @@ export class UserHomepageService {
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
-    console.error(errMsg);
     return Observable.throw(errMsg);
   }
-
 }
