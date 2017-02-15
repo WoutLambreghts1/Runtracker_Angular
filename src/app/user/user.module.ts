629efc8f488e255dcf327a21ca634f2d4352366b
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-15 10:20:59 +0100

    renaming kebab case

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,9 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {UserRoutingModule} from "./user-routing.module";
-import {EditProfileService} from "./edit/editprofile.service.ts";
+import {EditProfileService} from "./edit/edit-profile.service.ts";
 import {UserHomepageComponent} from "./home/userhomepage.component.ts";
-import {EditProfileComponent} from "./edit/editprofile.component.ts";
+import {EditProfileComponent} from "./edit/edit-profile.component.ts";
 import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
