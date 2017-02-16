9e1774aaf23accd6c3a21cb153ab9e7084be0cd9
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-16 16:02:37 +0100

    create competition screen

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {ChallengeService} from "./challenge.service";
 import {AuthService} from "./../authentication/auth.service";
+import {Competition} from "../model/competition";
 
 @Component({
   selector: 'challenge',
@@ -10,9 +11,13 @@ import {AuthService} from "./../authentication/auth.service";
 })
 
 export class ChallengeComponent{
-
+  private newCompetition: Competition;
   constructor(private challengeService: ChallengeService,private auth:AuthService){
+    this.newCompetition = new Competition();
+  }
 
+  private onClickAddCompetition(competition: Competition): void {
+    alert(competition.deadline);
   }
 
 }
