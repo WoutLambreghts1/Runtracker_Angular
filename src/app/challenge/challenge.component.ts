570eeb05355dc859cf023fb4d06ec74a420fd124
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-16 20:29:44 +0100

    create competition screen opmaak

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {ChallengeService} from "./challenge.service";
 import {AuthService} from "./../authentication/auth.service";
 import {Competition} from "../model/competition";
+import {Goal} from "../model/goal";
 
 @Component({
   selector: 'challenge',
@@ -12,18 +13,40 @@ import {Competition} from "../model/competition";
 
 export class ChallengeComponent{
   private newCompetition: Competition;
+  private goals: Goal[];
+  private competitions: Competition[];
   constructor(private challengeService: ChallengeService,private auth:AuthService){
-
   }
 
   ngOnInit(): void {
     this.newCompetition = new Competition();
     this.newCompetition.maxParticipants = 2;
     this.newCompetition.deadline = new Date();
+    this.newCompetition.competitionType="NOT_REALTIME";
+
+    this.challengeService.getGoals().subscribe(
+      (goals: Goal[]) => {
+        this.goals = goals;
+      },
+      error => {
+        console.log(error as string);
+      }
+    );
+
+    this.challengeService.getCompetitions().subscribe(
+      (competitions: Competition[]) => {
+        this.competitions = competitions;
+        console.log(competitions);
+      },
+      error => {
+        console.log(error as string);
+      }
+    );
   }
 
   private onClickAddCompetition(competition: Competition): void {
-    alert(competition.deadline);
+    alert("add comp");
+    this.challengeService.createCompetition(competition);
   }
 
 }
--- src/app/challenge/challenge.service.ts
+++ src/app/challenge/challenge.service.ts
@@ -1,7 +1,9 @@
-import {Http,Headers,Response} from '@angular/http';
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
 import {Observable} from "rxjs/Observable";
 import {Injectable} from '@angular/core';
 import * as myGlobals from "./../globals";
+import {Goal} from "../model/goal";
+import {Competition} from "../model/competition";
 
 @Injectable()
 export class ChallengeService {
@@ -12,6 +14,54 @@ export class ChallengeService {
     if(this.jwt) {
       this.authHeader.append('token', this.jwt);
     }
+  }
+
+  getGoals(): Observable<Goal[]>{
+    return this.http.get(myGlobals.BACKEND_BASEURL + '/api/goals')
+      .map((res: Response) =>  res.json())
+      .catch(this.handleErrorObservable);
+  }
+
+  getCompetitions(): Observable<Competition[]>{
+    return this.http.get(myGlobals.BACKEND_BASEURL + '/api/competitions',{
+        headers: this.authHeader
+      })
+      .map((res: Response) =>  res.json())
+      .catch(this.handleErrorObservable);
+  }
+
+  createCompetition(competition: Competition): Observable<any>{
+    this.authHeader.append('Content-Type', 'application/json');
+    let options = new RequestOptions({headers: this.authHeader});
+
+    console.log(options);
+
+    let newCompetition = {
+      "competitionType":"NOT_REALTIME",
+      "deadline":"2017-02-20",
+      "maxParticipants":4,
+      "goal":null,
+      "userCreated":null,
+      "userWon":null,
+      "trackings":[],
+      "usersRun":[]
+    }
+
+    return this.http.post(myGlobals.BACKEND_BASEURL + '/api/competitions/createCompetition', newCompetition, options)
+      .map((res: Response) => res.json()).catch(err => this.handleErrorObservable(err));
+  }
+
+
+  private handleErrorObservable(error: Response | any): Observable<any> {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    return Observable.throw(errMsg);
   }
 
 }
