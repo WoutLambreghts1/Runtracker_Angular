a44d9e2a0389b390c72308765afa64f8b67df8de
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-19 13:30:28 +0100

    opzetten friends

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,8 +1,31 @@
 import {Component} from "@angular/core";
+import {FriendsService} from "./friends.service";
+import {User} from "../model/user";
 @Component({
   selector: 'friends',
-  template: '<div>Friends</div>'
+  templateUrl: 'friends.component.html',
+  styleUrls: ['friends.component.css'],
+  providers: [FriendsService]
 })
 
 export class FriendsComponent {
+  private users: User[] = [];
+
+
+  constructor(private friendService:FriendsService) {
+  }
+
+  ngOnInit():void {
+    this.friendService.getUsers().subscribe(
+      (users) => {
+        this.users = users;
+        console.log(users);
+      },
+      error => {
+        console.log(error as string);
+      }
+    );
+  }
+
+
 }
---
+++ src/app/friends/friends.service.ts
@@ -0,0 +1,38 @@
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
+import {Observable} from "rxjs/Observable";
+import {Injectable} from '@angular/core';
+import * as myGlobals from "./../globals";
+import {User} from "../model/user";
+
+@Injectable()
+export class FriendsService {
+  private jwt = localStorage.getItem('id_token');
+  private authHeader = new Headers();
+
+  constructor(private http:Http){
+    if(this.jwt) {
+      this.authHeader.append('token', this.jwt);
+    }
+  }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get(myGlobals.BACKEND_BASEURL + '/api/users/allUsers',{
+        headers: this.authHeader
+      })
+      .map((res: Response) =>  res.json())
+      .catch(this.handleErrorObservable);
+  }
+
+  private handleErrorObservable(error: Response | any): Observable<any> {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    return Observable.throw(errMsg);
+  }
+
+}
