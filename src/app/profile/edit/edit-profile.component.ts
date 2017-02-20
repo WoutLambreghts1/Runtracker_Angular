5e61c18804d4f6130f3a062dc530129cdc5e5077
jellemannaerts <jelle.mannaerts@gmail.com>
2017-02-20 10:22:47 +0100

    edit profile errors

--- src/app/profile/edit/edit-profile.component.ts
+++ src/app/profile/edit/edit-profile.component.ts
@@ -10,35 +10,48 @@ import {AuthService} from "../../authentication/auth.service";
   providers: [EditProfileService]
 })
 
-export class EditProfileComponent{
+export class EditProfileComponent {
   private user;
   private errorMsg;
   private available: boolean;
-  private pictureURL:string;
+  private pictureURL: string;
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.user = this.editProfileService.getUser().subscribe((user: User) => this.user = user);
     this.onUsernameChange(this.user.username);
     this.pictureURL = JSON.parse(localStorage.getItem("profile")).picture;
   }
-  onUsernameChange(username:string): void{
-    this.editProfileService.checkUsernameAvailable(username).subscribe((val:boolean) => this.available = val);
+
+  onUsernameChange(event): void {
+    if (this.user.username == "") {
+      this.errorMsg = "Username can not be empty. Please enter a valid username."
+    } else {
+      this.editProfileService.checkUsernameAvailable(this.user.username).subscribe((val: boolean) => {
+          this.available = val;
+          if (!val) {
+            this.errorMsg = "Username not available. Please choose another username."
+          } else {
+            this.errorMsg = "";
+          }
+        }, err => console.log(err)
+      );
+    }
   }
 
-  onClickUpdateUser(user:User): void{
-    if(user.username != "" && this.available){
+  onClickUpdateUser(user: User): void {
+    if (user.username != "" && this.available) {
       this.errorMsg = "";
       this.user = this.editProfileService.updateUser(user).subscribe((user: User) => this.user = user);
-    }else {
-      if(user.username == ""){
-        this.errorMsg="Username can not be empty. Please enter a valid username."
-      }else{
-        this.errorMsg="Username not available. Please choose another username."
+    } else {
+      if (user.username == "") {
+        this.errorMsg = "Username can not be empty. Please enter a valid username."
+      } else {
+        this.errorMsg = "Username not available. Please choose another username."
       }
     }
   }
 
-  constructor(private editProfileService: EditProfileService,private auth:AuthService){
+  constructor(private editProfileService: EditProfileService, private auth: AuthService) {
 
   }
 
