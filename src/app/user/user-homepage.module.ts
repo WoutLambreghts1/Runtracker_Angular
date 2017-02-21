552da3bb4a56fcb3b3f17ac79e153b85fa550faa
jellemannaerts <jelle.mannaerts@gmail.com>
2017-02-21 21:15:14 +0100

    core info redone + start homepage-feed

--- src/app/user/user-homepage.module.ts
+++ src/app/user/user-homepage.module.ts
@@ -7,11 +7,12 @@ import {SharedModule} from "../shared/shared.module";
 import {UserHomepageService} from "./home/user-homepage.service";
 import {HeaderComponent} from "../homepage/header/header.component";
 import {CoreInfoComponent} from "./core-info/core-info.component";
+import {HomepageFeedComponent} from "./homepage-feed/homepage-feed.component";
 
 @NgModule({
   imports: [CommonModule, FormsModule, UserHomepageRoutingModule],
   providers: [UserHomepageService],
-  declarations: [UserHomepageComponent, CoreInfoComponent],
+  declarations: [UserHomepageComponent, CoreInfoComponent, HomepageFeedComponent],
 })
 
 export class UserHomepageModule {
