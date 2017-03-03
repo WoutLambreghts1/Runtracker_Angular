a48e7f29825a2d162dada6ab8f2fa3d7bf92bf9f
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-03-03 20:46:13 +0100

    aanpassing testen challenge

--- e2e/challenge.e2e-spec.ts
+++ e2e/challenge.e2e-spec.ts
@@ -21,10 +21,6 @@ describe('challenge', () => {
 
   });
 
-  //CompetitionModal
-  let competitionModal = element(by.id('competitionModal'));
-  let h1 = element(by.tagName('h1'));
-  let h2 = element(by.tagName('h2'));
 
   it('Should have a correct url', () => {
     browser.driver.sleep(2000);
@@ -32,9 +28,20 @@ describe('challenge', () => {
   });
 
   it('Should have all elements on page & correct values', () => {
+    browser.driver.sleep(2000);
+    let competitionModal = element(by.id('competitionModal'));
+    let h1 = element(by.tagName('h1'));
+    let h2 = element(by.tagName('h2'));
+    let playerInfo1 = element(by.id('player-one'));
+    let playerInfo2 = element(by.id('player-two'));
+    let challengeScreenComp = element(by.id('challenge-screen'));
+
     expect(competitionModal.isPresent()).toBeTruthy();
     expect(h1.isPresent()).toBeTruthy();
     expect(h2.isPresent()).toBeTruthy();
+    expect(playerInfo1.isPresent()).toBeTruthy();
+    expect(playerInfo2.isPresent()).toBeTruthy();
+    expect(challengeScreenComp.isPresent()).toBeTruthy();
 
     expect(h1.getText()).toBe('Live challenges');
     expect(h2.getText()).toContain('Watch people challenge each other!');
