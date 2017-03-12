c2dd3fb03e37033589ca216c3569c45c4c98d43e
jellemannaerts <jelle.mannaerts@gmail.com>
2017-03-12 13:55:46 +0100

    lokaal werken

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,3 +1,3 @@
 export const BACKEND_BASEURL = 'http://schadronds.synology.me:7070';
-export const FRONTEND_BASEURL = 'https://runtracker-ip2-frontend.herokuapp.com';
-//export const FRONTEND_BASEURL = 'http://localhost:4200';
+//export const FRONTEND_BASEURL = 'https://runtracker-ip2-frontend.herokuapp.com';
+export const FRONTEND_BASEURL = 'http://localhost:4200';
