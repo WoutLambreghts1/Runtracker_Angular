a1f99e17c3a68a6dba307bd9c5034fd089de1951
Jens Schadron <jensschadron@hotmail.com>
2017-03-14 01:43:09 +0100

    Backend op NAS verloopt nu over https

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,3 +1,7 @@
-export const BACKEND_BASEURL = 'http://schadronds.synology.me:7070';
+export const BACKEND_BASEURL = 'https://tomcat.schadronds.synology.me';
+//export const BACKEND_BASEURL = 'http://schadronds.synology.me:7070';
+//export const BACKEND_BASEURL = 'http://192.168.0.101:7070';
+//export const BACKEND_BASEURL = 'http://runtracker-ip2-backend.herokuapp.com';
+//export const BACKEND_BASEURL = 'http://localhost:8080';
 //export const FRONTEND_BASEURL = 'https://runtracker-ip2-frontend.herokuapp.com';
 export const FRONTEND_BASEURL = 'http://localhost:4200';
