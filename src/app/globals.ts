dc33e9241f539ea829d9fd572b874ff38162840e
jellemannaerts <jelle.mannaerts@gmail.com>
2017-03-16 15:15:54 +0100

    heroku deployment

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,5 +3,5 @@ export const BACKEND_BASEURL = 'https://tomcat.schadronds.synology.me';
 //export const BACKEND_BASEURL = 'http://192.168.0.101:7070';
 //export const BACKEND_BASEURL = 'http://runtracker-ip2-backend.herokuapp.com';
 //export const BACKEND_BASEURL = 'http://localhost:8080';
-//export const FRONTEND_BASEURL = 'https://runtracker-ip2-frontend.herokuapp.com';
-export const FRONTEND_BASEURL = 'http://localhost:4200';
+export const FRONTEND_BASEURL = 'https://runtracker-ip2-frontend.herokuapp.com';
+//export const FRONTEND_BASEURL = 'http://localhost:4200';
