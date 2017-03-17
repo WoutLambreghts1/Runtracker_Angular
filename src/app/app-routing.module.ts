e8dd8be2c9f8b114fa339fcb37ea2dd12e5d1c9f
jellemannaerts <jelle.mannaerts@gmail.com>
2017-03-17 13:41:51 +0100

    pageNotFound deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {Routes, RouterModule} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {HomePageComponent} from "./homepage/homepage.component";
-import {PageNotFoundComponent} from "./PageNotFound.component";
 import {AuthGuard} from "./authentication/authguard";
 
 export const routes: Routes = [
